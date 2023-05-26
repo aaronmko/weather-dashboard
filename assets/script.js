55f31bfd270ab99ee689aecb546449eb8e378606
minkiko@gmail.com <minkiko@gmail.com>
2023-05-25 22:59:23 -0500

    calling a function displayCity to display city information and weather dynamically

--- assets/script.js
+++ assets/script.js
@@ -25,10 +25,25 @@ function displayWeather(cityName) {
             var oneCallUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${currentData.coord.lat}&lon=${currentData.coord.lon}&appid=9dd332c2cdf5ad3eee158912aa75b747&units=imperial`;
             fetch(oneCallUrl)
                 .then(function (response) {
-                    return response.json();
+                    return response.json(); // takes function and translates into json
                 })
                 .then(function (fiveDayData) {
                     if (searchHistory.includes(currentData.name) === false) {
                         searchHistory.push(currentData.name);
                         localStorage.setItem("city", JSON.stringify(searchHistory));
                     }
+                    // display city information where it dynamically generates the weather
+                    displayCity();
+                    console.log(fiveDayData);
+                    presentDayWeather.innerHTML = `<ul>
+        <li class="title">${currentData.name} /<span> ${moment(
+                        currentData.dt,
+                        "X" 
+                    ).format(" MM/DD/YYYY")} </span></li> 
+        <li><img src ="http://openweathermap.org/img/wn/${currentData.weather[0].icon
+                        }@2x.png" /></li> 
+        <li>Temp: ${currentData.main.temp}</li>
+        <li>Wind: ${currentData.wind.speed}</li>
+        <li>Humidity: ${currentData.main.humidity}</li>
+        <li>UV: <span style="background-color: green; color: white;"> ${fiveDayData.current.uvi
+                        }</span></li>
