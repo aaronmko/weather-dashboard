165d40a697af58f4681f2a16834fe28b0662ca07
minkiko@gmail.com <minkiko@gmail.com>
2023-05-25 20:21:37 -0500

    global variables created

---
+++ assets/script.js
@@ -0,0 +1,6 @@
+var cityInputEl = document.querySelector("#city-input");
+var searchForm = document.querySelector("#search-form");
+var clearButton = document.querySelector("#clear-history-button");
+var searchHistory = document.querySelector("#search-history");
+var presentDayWeather = document.querySelector("#present-day-weather");
+var fiveDayForecast = document.querySelector("#five-day-forecast");
