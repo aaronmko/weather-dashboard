a9e8d4f1fef2e7ef024d6e06bfef169b64d7403a
minkiko@gmail.com <minkiko@gmail.com>
2023-05-25 20:24:57 -0500

    created display function

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,14 @@
-var cityInputEl = document.querySelector("#city-input");
+// global variables
+var cityInput = document.querySelector("#city-input");
 var searchForm = document.querySelector("#search-form");
 var clearButton = document.querySelector("#clear-history-button");
 var searchHistory = document.querySelector("#search-history");
 var presentDayWeather = document.querySelector("#present-day-weather");
 var fiveDayForecast = document.querySelector("#five-day-forecast");
+
+// function to display the dashboard
+function dashboard(event) {
+    event.preventDefault();
+    var cityName = cityInput.value;
+    displayWeather(cityName);
+}
